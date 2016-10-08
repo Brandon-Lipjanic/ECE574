08c6bc3e7c53887052e0c146eda3a4bdfe137bff
Nick Sheptock <nsheptock@email.arizona.edu>
2016-10-08 14:19:09 -0700

    Add files via upload
    its better

--- Assignment2/Main.cpp
+++ Assignment2/Main.cpp
@@ -4,14 +4,18 @@
 #include <string>
 
 int main() {
-
+	int i;
 	vector<string> v;
 	vector<string> v1;
+	vector<vector<string>> master;
 
 	v = readFile("474a_circuit3.txt");
 
-	v1 = getInputs(v[0]);
-
+	for (i = 0; i < v.size(); ++i) {
+		v1 = getInputs(v[i]);
+		if(v1.size() != 0)
+			master.push_back(v1);
+	}
 
 	return 0;
 }
